13480cd0774ebe6e6732f36864d017ffd203ce9d
144786 <71454110+144786@users.noreply.github.com>
2020-11-09 23:06:24 -0500

    Create part_5.py

---
+++ sample code/part_5.py
@@ -0,0 +1,10 @@
+# Welcome to part 5 to the programming skills validation task!
+
+# This is the fifth and final part of your program. You will now combine the functionality of all the moduled you
+# have created to write a program which takes in a directory of emails, and does the following:
+    # - write a test file of all links found.
+    # - check every link with VirusTotal and write a list of all malicious links to another text file.
+    # - extract all attachements to a folder.
+    # - hash every attachment found and send the hash to Virus Total. Save all the malicious files to another folder.
+
+# Good luck!
