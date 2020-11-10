b28f43ff09db054a90b8a610e46ff802e4ec5fdc
144786 <71454110+144786@users.noreply.github.com>
2020-11-09 23:30:43 -0500

    Create link_scraper.py

---
+++ assignments/starter code/link_scraper.py
@@ -0,0 +1,17 @@
+# HINT: The DS solution contains following:
+from urlextract import URLExtract
+import mailparser
+from os import walk
+
+
+def scrape_links(emails_folder : str):
+    """
+    Take in a string containing a directory of emails. For each email, find all the links in the body, then write all
+    the links found into a text file.
+
+    Args:
+        emails_folder (str):
+        A string containing a directory of emails.
+    """
+    # TODO
+    pass
