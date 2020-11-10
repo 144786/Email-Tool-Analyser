814ad19b37d92628390ae24e2e558dbd47ee2f5b
144786 <71454110+144786@users.noreply.github.com>
2020-11-09 23:29:21 -0500

    Create attachment_getter.py

---
+++ assignments/starter code/attachment_getter.py
@@ -0,0 +1,17 @@
+# HINT: in the DS solution, the following are used.
+from os import walk
+from os import path # before you create a direcory, use this the check if it's already there
+from os import mkdir
+import mailparser
+
+def get_attachements(email_folder : str):
+    """
+    Get all of the emails in the folder locate at the path provided, get the emails, get their attachments, 
+    and store the attachments in a folder called 'found attachments
+
+    Args:
+        email_folder (str): 
+        The path to the folder that contains all the emails.
+    """
+# TODO
+pass
