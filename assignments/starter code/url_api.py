c2a4c1c468b4207a974b045e98c553b437cb8212
144786 <71454110+144786@users.noreply.github.com>
2020-11-09 23:31:10 -0500

    Create url_api.py

---
+++ assignments/starter code/url_api.py
@@ -0,0 +1,17 @@
+def url_is_malicious(url_to_check : str) -> bool:
+    """
+    Take the URL provided, send it VirusTotal, and report whether comes back flagged as malware. 
+
+    ***NOTE: to interact with VT's API, you will need an API key. Here it is:
+    '3b6f919f8590692ff02fd1bd7136e2dc02ef1b2ccfe5392745ab5770d6c573a2'
+
+    Args:
+        file_path (string): 
+        a string containing the URL to be checked by VirusTotal.
+
+    Returns:
+        bool: 
+        True if VirusTotal flags the URL as malicious, False otherwise.
+    """
+    # TODO
+    pass
